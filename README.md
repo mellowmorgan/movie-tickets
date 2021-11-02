82954e83c04cc83f7056db3f126cb16ea4a8bcba
mellowmorgan <morgan.waites@gmail.com>
2021-11-02 14:50:14 -0700

    Add getPrice function
    Co-authored-by: Jeff Terrell <jeffaterrell@gmail.com>

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 Describe: Ticket(movie,date,time,age)
 Test: It should save selected movie, date, time, and age to Ticket
-Code: let movieticket = new Ticket("venom", 11/02/2021, 1800, 18);
+Code: let movieticket = new Ticket("venom", 2021-11-02, 6:00 PM, 18);
 movieTicket.title;
 movieTicket.date;
 movieTicket.time;
 movieTicket.age;
 Expected Output: "venom", 2021-11-02, 6:00 PM, 18
+
+
+Describe Ticket.prototype.checkMatinee()
+Test: It should check user inputted time for matinee and return true
+Code: ticket.checkMatinee()
+Expected Output: true
+
+Test: It should check user inputted time for matinee and return false
+Code: ticket.checkMatinee()
+Expected Output: false
+
+Describe: Ticket.prototype.getPrice()
+Test: "It should check age, if matinee, release new or not, return price amount."
+Code:
+ticket.getPrice();
+Expected Output: $10
+
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,41 @@
+function Ticket (title, date, time, age) {
+  this.title = title;
+  this.date = date;
+  this.time = time;
+  this.age = age;
+  this.price = 8;
+}
+
+Ticket.prototype.getPrice = function(){
+  if ((this.title!="promisingyoungwoman") && (this.title!="roadrunner")){
+    this.price+=2;
+  }
+  if (this.checkMatinee()){
+    this.price-=2;
+  }
+  if ((this.age>=55)||(this.age<=12)){
+    this.price-=2;
+  }
+  return this.price;
+}
+
+Ticket.prototype.checkMatinee = function(){
+  if ((this.time==="6:00 PM") || (this.time==="7:00 PM") || (this.time==="8:00 PM") || (this.time==="9:00 PM") || (this.time==="10:00 PM")) {
+      return false;
+  }
+  return true;
+}
+
+
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
-    alert($("#movieTime").val());
+    const movieTitle = $("#movies").val();
+    const date = $("#date").val();
+    const time =$("#movieTime").val();
+    const age = parseInt($("#age").val());
+    let ticket = new Ticket(movieTitle, date, time, age);
+    const price = ticket.getPrice();
+    $("#cart").text("Ticket 1: $"+price);
   });
 });
